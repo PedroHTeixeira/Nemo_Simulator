8036d0c9fb002300750101845b7be9a587e19643
Pedro  Teixeira <pedroteixeir@me.com>
2020-11-26 00:01:52 -0300

    Update Follower

--- nemo_simulator/scripts/follower.py
+++ nemo_simulator/scripts/follower.py
@@ -10,23 +10,33 @@ def callback(msg):
     z = msg.point.z
     return follower(x,y,z)
 
-def follower(x,y,z):
+def follower(x,y,z,g=0):
     pub = rospy.Publisher('cmd_vel',Twist, queue_size=10)
     move_nemo= Twist()
-    if x > 3:
-        move_nemo.linear.x=1
-    elif x < -3:
-        move_nemo.linear.x=-1
-    else:
-        move_nemo.linear.x=0
     
-    if y > 3:
+    if x > 3:
+        move_nemo.angular.z=-1
+        pub.publish(move_nemo)
+        time.sleep(1)
+        move_nemo.angular.z=0
+        move_nemo.linear.y=1
+        elif y < -3:
+            move_nemo.linear.y=-1
+        time.sleep(1)
+    elif x < -3:
+        move_nemo.angular.z=1
+        pub.publish(move_nemo)
+        time.sleep(1)
+        move_nemo.angular.z=0
+        move_nemo.linear.y=1
+        elif y < -1:
+            move_nemo.linear.y=-1
+        pub.publish(move_nemo)
+        time.sleep(1)
+    elif y > 3:
         move_nemo.linear.y=1
     elif y < -3:
         move_nemo.linear.y=-1
-    else:
-        move_nemo.linear.y=0
-
     if z>0:
         move_nemo.linear.z=-1
     else:
