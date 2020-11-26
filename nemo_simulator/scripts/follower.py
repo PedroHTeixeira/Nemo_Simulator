6c85219d8bf52ecb8c35e19cc8b167cd6e67851a
Pedro  Teixeira <pedroteixeir@me.com>
2020-11-26 13:45:26 -0300

    Update Follower

--- nemo_simulator/scripts/follower.py
+++ nemo_simulator/scripts/follower.py
@@ -10,7 +10,7 @@ def callback(msg):
     z = msg.point.z
     return follower(x,y,z)
 
-def follower(x,y,z,g=0):
+def follower(x,y,z):
     pub = rospy.Publisher('cmd_vel',Twist, queue_size=10)
     move_nemo= Twist()
     
@@ -20,30 +20,35 @@ def follower(x,y,z,g=0):
         time.sleep(1)
         move_nemo.angular.z=0
         move_nemo.linear.y=1
-        elif y < -3:
-            move_nemo.linear.y=-1
-        time.sleep(1)
+        if y < -3:
+            move_nemo.linear.y=1
+        pub.publish(move_nemo)
+        time.sleep(2)
     elif x < -3:
         move_nemo.angular.z=1
         pub.publish(move_nemo)
         time.sleep(1)
         move_nemo.angular.z=0
         move_nemo.linear.y=1
-        elif y < -1:
-            move_nemo.linear.y=-1
+        if y < -2:
+            move_nemo.linear.y=1
         pub.publish(move_nemo)
-        time.sleep(1)
-    elif y > 3:
-        move_nemo.linear.y=1
-    elif y < -3:
-        move_nemo.linear.y=-1
-    if z>0:
+        time.sleep(2)
+    else:
+        move_nemo.linear.y=0
+    if z>3:
         move_nemo.linear.z=-1
+        move_nemo.linear.y=-1
     else:
         move_nemo.linear.z=0
+        if y < -3:
+            move_nemo.linear.y=1
+        elif y > 3:
+            move_nemo.linear.y=1
     
     pub.publish(move_nemo)
+
 rospy.init_node('follow')
-time.sleep(18)
+time.sleep(20)
 rospy.Subscriber("/sonar_data", PointStamped, callback)
 rospy.spin()
