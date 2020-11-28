0cfcf45f678e984ac6733653aaf5a4de8833e6be
Pedro  Teixeira <pedroteixeir@me.com>
2020-11-28 14:50:57 -0300

    Find Points

--- nemo_simulator/scripts/camera.py
+++ nemo_simulator/scripts/camera.py
@@ -6,6 +6,7 @@ from cv_bridge import CvBridge
 import time
 import numpy as np
 from geometry_msgs.msg import PointStamped
+from std_msgs.msg import Float64MultiArray 
 
 def callback(msg):
     rate = rospy.Rate(10)
@@ -19,7 +20,13 @@ def callback(msg):
     mask = cv2.erode(mask, None, iterations=2)
     dimensions=mask.shape
     contour_mask, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    contour_mask_2, contours_2, hierarchy_2 = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    pub0=rospy.Publisher("contours",Float64MultiArray,queue_size=10)
+    message =Float64MultiArray() 
+    message.data = contours_2
+    pub0.publish(message)
     blank_image = np.zeros((dimensions[0],dimensions[1],3), np.uint8)
+    img_lucca = cv2.drawContours(blank_image, contours_2, -1, (255,255,255), 3)
     img_ctr=cv2.drawContours(blank_image, contours, -1, (255,255,255), 3)
     img_ctr = cv2.cvtColor(img_ctr, cv2.COLOR_BGR2GRAY)
     m=cv2.moments(img_ctr,False)
@@ -35,8 +42,10 @@ def callback(msg):
         cv2.putText(cv_image,'Red Not Found',(10,500), cv2.FONT_HERSHEY_SIMPLEX, 4,(0,0,255),6,cv2.LINE_AA)
     pub=rospy.Publisher("camera/image_interpreted",Image,queue_size=10)
     pub3=rospy.Publisher("camera/image_masked_contours",Image,queue_size=10)
+    pub5=rospy.Publisher("camera/image_masked_corners",Image,queue_size=10)
     pub4=rospy.Publisher("camera/image_masked",Image,queue_size=10)
     pub.publish(bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+    pub5.publish(bridge.cv2_to_imgmsg(img_lucca, "mono8"))
     pub3.publish(bridge.cv2_to_imgmsg(img_ctr,"mono8"))
     pub4.publish(bridge.cv2_to_imgmsg(mask,"mono8"))
     
